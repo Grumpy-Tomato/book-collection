44ad8f7db98175a5977dc1e942b9c587abc07280
Grumpy-Tomato <cormier.tanner@gmail.com>
2023-12-12 02:36:49 -0400

    - created function to add objects in my list to the DOM in card form;

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = `${this.title} by ${this.author}, ${this.pages} pages.`;
+    this.cardContent = `<div class="card">
+    <h3 class="title">"${this.title}"</h3>
+    <p class="Author">${this.author}</p>
+    <p class="Pages">${this.pages} Pages</p>
+    <div class="card-bottom">
+        <button class="${this.read}">${this.read}</button>
+        <button class="delete">
+            <img src="./images/admin-dashboard-icons.svg" height="100%" width="100%" alt="">
+        </button>
+    </div>
+    </div>`;
     
 }
 
@@ -13,11 +24,20 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const doAndroidsDream = new Book('Do Androids Dream of Electric Sheep?',"Philip K. Dick", 123, true);
-const electricDreams = new Book('Electric Dreams', 'Philip K. Dick', 323, true);
-const harryPotter = new Book("Harry Potter", "J.K. Rowling", 486, true);
+const doAndroidsDream = new Book('Do Androids Dream of Electric Sheep?',"Philip K. Dick", 123, 'read');
+const electricDreams = new Book('Electric Dreams', 'Philip K. Dick', 323, 'read');
+const harryPotter = new Book("Harry Potter", "J.K. Rowling", 486, 'read');
 addBookToLibrary(harryPotter);
 addBookToLibrary(doAndroidsDream);
 addBookToLibrary(electricDreams);
 
 console.log(myLibrary);
+
+// Dom Objects
+const addCard = document.querySelector('.card-add')
+
+for (i in myLibrary) {
+    let bookObject = myLibrary[i];
+    addCard.insertAdjacentHTML("beforebegin", bookObject.cardContent);
+}
+
